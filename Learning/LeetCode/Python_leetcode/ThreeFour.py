9582af91f6e41148cfe5eefe3668f92a441a95f6
Cetacean <1030wendy@sina.com>
2022-03-30 23:24:42 +0800

    LeetCode 二分法 34 区块划分

--- Learning/LeetCode/Python_leetcode/ThreeFour.py
+++ Learning/LeetCode/Python_leetcode/ThreeFour.py
@@ -3,14 +3,31 @@ from typing import List
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        low, high = 0, target
-        ans = List[int]
+        low, high = 0, len(nums) - 1
+        pos = -1
         while low <= high:
             mid = (low + high) // 2
-            if List[mid] < target:
-                mid = high + 1
-            elif List[mid] > target:
-                mid = low + 1
+            if nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
             else:
-                pass
+                pos = mid
+                break
+        if pos == -1:
+            return [-1, -1]
+        else:
+            begin = end = pos
+            while (begin - 1) > -1 and nums[begin - 1] == target:
+                begin -= 1
+            while (end + 1) < len(nums) and nums[end + 1] == target:
+                end += 1
+        return [begin, end]
 
+
+if __name__ == "__main__":
+    s = Solution()
+    # nList = [5, 7, 7, 8, 8, 10]
+    test1 = [1,1,2]
+
+    print(s.searchRange(test1, 1))
