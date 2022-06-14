e98c22b13a6ada680ee30d5497744b6b5c82ebed
Cetacean <1030wendy@sina.com>
2022-06-14 18:31:45 +0800

    update 347 桶排序 使用了Map方法解决了昨天的问题
    update 451 桶排序
    新使用了StringBuffer, toCharString方法

--- Learning/Algorithm/Java/src/main/java/Sort/Sorts.java
+++ Learning/Algorithm/Java/src/main/java/Sort/Sorts.java
@@ -10,9 +10,15 @@ import org.junit.Test;
 
 public class Sorts {
     public static void main(String[] args) {
-        int[] nums = {1,1,1,1,2,2,3,4};
-        int k = 2;
-        _347_BuketSort buketSort = new _347_BuketSort();
-        buketSort.topKFrequent(nums,k);
+//        基础桶排序：347
+//        int[] nums = {1,1,1,1, -1,-1,2,2,2,3,4,4,};
+//        int k = 4;
+//        _347_BuketSort buketSort = new _347_BuketSort();
+//        buketSort.topKFrequent(nums,k);
+
+//        进阶桶排序：451
+        String s = "aabcdA";
+        _451_BuketSort2 buketSort2 = new _451_BuketSort2();
+        buketSort2.frequencySort(s);
     }
 }
